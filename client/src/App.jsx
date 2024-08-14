f41224e8f2010a8949926c27f4cae254e556d008
aresstarc <gauravdik9287@gmail.com>
2024-08-14 12:31:09 +0530

    Updated client with DaisyUI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+// import Login from './pages/login/Login'
+// import SignUp from './pages/signup/Signup'
+import Home from './pages/home/Home'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className='p-4 h-screen flex items-center justify-center'>
+      {/* <Login /> */}
+      {/* <SignUp /> */}
+      <Home />
+    </div>
   )
 }
 
